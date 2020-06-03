76819906e86e2624a9b59fd15ac4907ad09185b8
Josh Santangelo <josh@endquote.com>
2020-06-02 20:03:59 -0700

    mail script ported to typescript

---
+++ scripts/mail/index.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import Handlebars from "handlebars";
+import { inspect } from "util";
+import { issues, query } from "../../data/issues";
+
+// node index.ts [baseUrl] [issueId]
+
+const baseUrl = process.argv[2] || "http://endquote.com";
+
+const id = process.argv[3] ? process.argv[3] : issues[0].id;
+const issue = query(id);
+
+const html = fs.readFileSync("template.hbs").toString();
+
+Handlebars.registerHelper("stylelink", (s) =>
+  s.replace("<a ", '<a style="color:black" ')
+);
+
+const template = Handlebars.compile(html);
+
+console.log(inspect({ issue, baseUrl }));
+
+const out = template({ issue, baseUrl });
+
+fs.writeFileSync("email.html", out);
