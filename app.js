b3e1cce737f53842b6f08e685ecd145086427322
Josh Santangelo <josh@endquote.com>
2020-06-09 21:49:45 -0700

    debug

--- app.js
+++ app.js
@@ -6,16 +6,22 @@ const port = process.env.PORT || 3000;
 const { DEV, PROD_HREF } = require("./data/constants");
 const app = next({ DEV });
 const handle = app.getRequestHandler();
+const fs = require("fs");
+const path = require("path");
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
-    if (parsedUrl.protocol !== "https:" && !DEV) {
-      res.writeHead(301, { Location: `${PROD_HREF}${parsedUrl.href}` });
-      res.end();
-    } else {
-      handle(req, res, parsedUrl);
-    }
+    // if (parsedUrl.protocol !== "https:" && !DEV) {
+    //   res.writeHead(301, { Location: `${PROD_HREF}${parsedUrl.href}` });
+    //   res.end();
+    // } else {
+    fs.appendFileSync(
+      path.join(__dirname, "log.txt"),
+      JSON.stringify(parsedUrl)
+    );
+    handle(req, res, parsedUrl);
+    // }
   }).listen(port, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
