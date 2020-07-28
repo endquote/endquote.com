32336c3cda6e12268f650d81d43b0580c15e3fb8
Josh Santangelo <josh@endquote.com>
2020-07-28 06:40:02 -0700

    redirects are no longer experimental?

--- next.config.js
+++ next.config.js
@@ -27,84 +27,82 @@ module.exports = withBundleAnalyzer({
     return config;
   },
 
-  experimental: {
-    async redirects() {
-      let routes = [
-        {
-          // /news > /newsletter
-          source: "/news",
-          destination: "/newsletter",
-        },
-        {
-          // /issue1 > /issue/1
-          source: "/issue:id",
-          destination: "/issue/:id",
-        },
-        {
-          // /newsletter/1 > /issue/1
-          source: "/newsletter/:id",
-          destination: "/issue/:id",
-        },
-        {
-          // /news/1 > /issue/1
-          source: "/news/:id",
-          destination: "/issue/:id",
-        },
-        {
-          // images/issues/6/origami.jpg > images/newsletter/6/origami.jpg
-          source: "/images/newsletter/:issue/:image",
-          destination: "/images/issues/:issue/:image",
-        },
-      ];
+  async redirects() {
+    let routes = [
+      {
+        // /news > /newsletter
+        source: "/news",
+        destination: "/newsletter",
+      },
+      {
+        // /issue1 > /issue/1
+        source: "/issue:id",
+        destination: "/issue/:id",
+      },
+      {
+        // /newsletter/1 > /issue/1
+        source: "/newsletter/:id",
+        destination: "/issue/:id",
+      },
+      {
+        // /news/1 > /issue/1
+        source: "/news/:id",
+        destination: "/issue/:id",
+      },
+      {
+        // images/issues/6/origami.jpg > images/newsletter/6/origami.jpg
+        source: "/images/newsletter/:issue/:image",
+        destination: "/images/issues/:issue/:image",
+      },
+    ];
 
-      for (const p of projects) {
-        // /project_name > /project/project_name
+    for (const p of projects) {
+      // /project_name > /project/project_name
+      routes.push({
+        source: `/${p.id}`,
+        destination: `/project/${p.id}`,
+      });
+
+      if (p.id.includes("_")) {
+        // /projectname > /project/project_name
         routes.push({
-          source: `/${p.id}`,
+          source: `/${p.id.replace(/_/g, "")}`,
           destination: `/project/${p.id}`,
         });
 
-        if (p.id.includes("_")) {
-          // /projectname > /project/project_name
-          routes.push({
-            source: `/${p.id.replace(/_/g, "")}`,
-            destination: `/project/${p.id}`,
-          });
-
-          // /project/projectname > /project/project_name
-          routes.push({
-            source: `/project/${p.id.replace(/_/g, "")}`,
-            destination: `/project/${p.id}`,
-          });
-        }
-      }
-
-      for (const p of posts) {
-        // /post_name > /post/post_name
+        // /project/projectname > /project/project_name
         routes.push({
-          source: `/${p.id}`,
+          source: `/project/${p.id.replace(/_/g, "")}`,
+          destination: `/project/${p.id}`,
+        });
+      }
+    }
+
+    for (const p of posts) {
+      // /post_name > /post/post_name
+      routes.push({
+        source: `/${p.id}`,
+        destination: `/post/${p.id}`,
+      });
+
+      if (p.id.includes("_")) {
+        // /postname > /post/post_name
+        routes.push({
+          source: `/${p.id.replace(/_/g, "")}`,
           destination: `/post/${p.id}`,
         });
 
-        if (p.id.includes("_")) {
-          // /postname > /post/post_name
-          routes.push({
-            source: `/${p.id.replace(/_/g, "")}`,
-            destination: `/post/${p.id}`,
-          });
-
-          // /post/postname > /post/post_name
-          routes.push({
-            source: `/post/${p.id.replace(/_/g, "")}`,
-            destination: `/post/${p.id}`,
-          });
-        }
+        // /post/postname > /post/post_name
+        routes.push({
+          source: `/post/${p.id.replace(/_/g, "")}`,
+          destination: `/post/${p.id}`,
+        });
       }
+    }
 
-      // Add required "permanent" property
-      routes = routes.map((r) => ({ ...r, permanent: true }));
+    // Add required "permanent" property
+    routes = routes.map((r) => ({ ...r, permanent: true }));
 
-      return routes;
-    },
+    return routes;
   },
 });
