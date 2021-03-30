e1feaa7a76d6263a541455358e9c9be2ba49c72d
Josh Santangelo <josh@endquote.com>
2021-03-29 17:36:47 -0700

    next 10.1

--- next.config.js
+++ next.config.js
@@ -9,6 +9,11 @@ module.exports = withBundleAnalyzer({
   poweredByHeader: false,
   reactStrictMode: true,
 
+  // https://nextjs.org/docs/messages/webpack5
+  future: {
+    webpack5: true,
+  },
+
   webpack: (config, options) => {
     // SVG support. https://react-svgr.com
     config.module.rules.push({
