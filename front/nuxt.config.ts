22e0f4765f1407cc3e2b7f8229943a6c1ac6343d
endquote <josh@endquote.com>
2025-01-30 15:25:25 -0800

    test passing env

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -55,7 +55,9 @@ export default defineNuxtConfig({
     "/do/**": { proxy: "https://endquote.objects-us-east-1.dream.io/**" },
   },
   runtimeConfig: {
+    apiSecret: "", // can be overridden by NUXT_API_SECRET environment variable
     public: {
+      apiBase: "", // can be overridden by NUXT_PUBLIC_API_BASE environment variable
       hostname: "localhost",
     },
   },
