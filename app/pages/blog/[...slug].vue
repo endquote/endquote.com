66f694321a613c73d6e6c7207e594f3b07eaff81
endquote <josh@endquote.com>
2025-02-09 19:43:35 -0800

    dates are the worst

--- app/pages/blog/[...slug].vue
+++ app/pages/blog/[...slug].vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 const route = useRoute();
-const { data: page } = await useAsyncData(() =>
-  queryCollection("blog")
+const { data: page } = await useAsyncData(() => {
+  if (useDev()) {
+    return queryCollection("blog").where("slug", "=", route.path.split("/")[2]).first();
+  }
+
+  return queryCollection("blog")
     .where("slug", "=", route.path.split("/")[2])
     .where("draft", "=", false)
     .where("date", "<", new Date().toISOString())
-    .first(),
-);
+    .first();
+});
 
 if (!page.value) {
   throw createError({ statusCode: 404 });
--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,28 +1,33 @@
 <script setup lang="ts">
 import { useDateFormat } from "@vueuse/core";
-
 const route = useRoute();
 const { data: page } = await useAsyncData(() => queryCollection("content").path(route.path).first());
 useSiteHead(page.value);
 
-const { data: posts } = await useAsyncData(() =>
-  queryCollection("blog")
-    .where("draft", "=", false)
-    .where("date", "<", new Date().toISOString())
-    .order("date", "DESC")
-    .all(),
-);
+const now = ref(new Date().toISOString());
+
+const { data: posts } = await useAsyncData(() => {
+  if (useDev()) {
+    return queryCollection("blog").order("date", "DESC").all();
+  }
+  return queryCollection("blog").where("draft", "=", false).where("date", "<", now.value).order("date", "DESC").all();
+});
 </script>
 
 <template>
   <div class="prose-custom">
     <ContentRenderer v-if="page" :value="page" />
-    <div v-for="post in posts" :key="post.slug" class="flex">
-      <div class="mr-4 text-nowrap">{{ useDateFormat(post.date, "MMM D, YYYY") }}</div>
-      <div>
-        <a :href="`/blog/${post.slug}`"
-          ><strong>{{ post.title }}</strong> - {{ post.subtitle }}</a
-        >
+    <div
+      v-for="post in posts"
+      class="table-row w-full"
+      :key="post.slug"
+      :class="{ 'bg-amber-400': post.draft || post.date > now }"
+    >
+      <div class="table-cell text-nowrap pr-7">{{ useDateFormat(post.date, "MMM D, YYYY") }}</div>
+      <div class="table-cell w-full">
+        <a :href="`/blog/${post.slug}`">
+          <strong>{{ post.title }}</strong> - {{ post.subtitle }}
+        </a>
       </div>
     </div>
   </div>
