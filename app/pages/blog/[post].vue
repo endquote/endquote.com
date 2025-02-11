61d15374840f7af922d5d73d8f4a0e6342ed4778
endquote <josh@endquote.com>
2025-02-10 20:49:50 -0800

    add date formatting and location display to blog post component

--- app/pages/blog/[post].vue
+++ app/pages/blog/[post].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useDateFormat } from "@vueuse/core";
+
 const route = useRoute();
 
 const { data: page } = await useAsyncData(() => {
@@ -21,7 +23,16 @@ useSiteHead(page.value);
 </script>
 
 <template>
-  <div class="prose-custom">
+  <div class="prose-custom" v-if="page">
+    <div class="flex flex-col md:flex-row mb-2">
+      <div class="flex-1">
+        {{ useDateFormat(page.date, "h:mma, MMM D, YYYY") }}
+      </div>
+      <div class="flex-shrink-0">
+        {{ page.location }}
+      </div>
+    </div>
+
     <ContentRenderer v-if="page" :value="page" />
   </div>
 </template>
