ddc8e43e24febda43ef5dc597ce696afe808826b
endquote <josh@endquote.com>
2025-02-17 09:36:59 -0800

    don't change thumb text color in dark mode

--- app/components/ProjectThumb.vue
+++ app/components/ProjectThumb.vue
@@ -10,19 +10,12 @@ const props = defineProps<{
 <template>
   <NuxtLink :to="`/${project.stem}`" class="group relative block overflow-hidden rounded-lg">
     <div>
-      <Image
-        :src="`/do/${project.stem}/project.jpg`"
-        width="640"
-        height="426"
-        sizes="640px sm:560px md:330px lg:296px xl:381px 2xl:467px"
-        :alt="project.title"
-        class="w-full"
-      />
+      <Image :src="`/do/${project.stem}/project.jpg`" width="640" height="426"
+        sizes="640px sm:560px md:330px lg:296px xl:381px 2xl:467px" :alt="project.title" class="w-full" />
     </div>
     <div class="absolute inset-0 bg-white/90 opacity-0 transition-all duration-300 ease-out group-hover:opacity-100">
       <div
-        class="pointer-events-none flex h-full w-full translate-y-1/3 flex-col items-center justify-center px-4 transition-all duration-300 ease-out group-hover:translate-y-0"
-      >
+        class="pointer-events-none flex h-full w-full translate-y-1/3 flex-col items-center justify-center px-4 transition-all duration-300 ease-out group-hover:translate-y-0 text-[var(--ui-color-neutral-700)]">
         <div class="mb-1 text-center text-2xl font-bold">{{ project.title }}</div>
         <div class="text-center">{{ project.subtitle }}</div>
       </div>
