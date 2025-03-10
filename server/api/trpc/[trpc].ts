95f96318c92d3e5b8ccf43f676ca9a370a4bdb1d
endquote <josh@endquote.com>
2025-03-09 20:45:26 -0700

    more consistent trip objects from the api

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -1,14 +1,75 @@
+import type { Prisma } from "@prisma/client";
 import { createNuxtApiHandler } from "trpc-nuxt";
 import { z } from "zod";
 import useDev from "~/composables/useDev";
 import prisma from "~~/lib/prisma";
 import { publicProcedure, router } from "~~/server/trpc/trpc";
 
+// use the same select query for both trips queries
+const tripSelect = {
+  eqId: true,
+  start: true,
+  end: true,
+  checkins: {
+    select: {
+      eqId: true,
+      date: true,
+      flight: {
+        select: {
+          fromAirport: true,
+          toAirport: true,
+          canceled: true,
+        },
+      },
+      venue: {
+        select: {
+          fsId: true,
+          name: true,
+          airport: true,
+          restaurant: {
+            select: {
+              award: true,
+              url: true,
+            },
+          },
+        },
+      },
+    },
+    orderBy: { date: "asc" },
+  },
+  flights: {
+    select: {
+      eqId: true,
+      canceled: true,
+      date: true,
+      fromAirport: true,
+      toAirport: true,
+      actualDeparture: true,
+      scheduledDeparture: true,
+    },
+  },
+} satisfies Prisma.tripSelect;
+
+// process the trip result before returning
+const processTrip = (trip: Prisma.tripGetPayload<{ select: typeof tripSelect }>) => {
+  return {
+    ...trip,
+    flights: trip.flights.map((flight) => ({
+      eqId: flight.eqId,
+      canceled: flight.canceled,
+      fromAirport: flight.fromAirport,
+      toAirport: flight.toAirport,
+      // make a canonical date for display
+      date: (flight.actualDeparture || flight.scheduledDeparture || flight.date).toISOString(),
+    })),
+  };
+};
+
 export const appRouter = router({
   // return all trips that have an associated page
   // in dev mode, return all trips
   trips: publicProcedure.query(async () => {
-    let where = {};
+    let where = {} satisfies Prisma.tripWhereInput;
     if (!useDev()) {
       const pages = await queryCollection(useEvent(), "trips").select("date").all();
       const pageDates = pages.map((page) => page.date);
@@ -22,14 +83,13 @@ export const appRouter = router({
       };
     }
 
-    return prisma.trip.findMany({
+    const trips = await prisma.trip.findMany({
       where,
-      include: {
-        checkins: { include: { venue: true }, orderBy: { date: "desc" } },
-        flights: { orderBy: { date: "desc" } },
-      },
+      select: tripSelect,
       orderBy: { start: "desc" },
     });
+
+    return trips.map(processTrip);
   }),
 
   // return a single trip by start date
@@ -48,17 +108,8 @@ export const appRouter = router({
         }
       }
 
-      return prisma.trip.findFirst({
-        include: {
-          checkins: {
-            include: {
-              flight: { select: { fromAirport: true, toAirport: true, canceled: true } },
-              venue: { include: { restaurant: { select: { award: true, url: true } } } },
-            },
-            orderBy: { date: "asc" },
-          },
-          flights: true,
-        },
+      const trip = await prisma.trip.findFirst({
+        select: tripSelect,
         orderBy: { start: "asc" },
         where: {
           start: {
@@ -67,6 +118,12 @@ export const appRouter = router({
           },
         },
       });
+
+      if (!trip) {
+        return null;
+      }
+
+      return processTrip(trip);
     }),
 });
 
