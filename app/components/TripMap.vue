c13ef61b5bd1a76d1e68ff413fafb888004e73a1
endquote <josh@endquote.com>
2025-03-30 15:54:46 -0700

    clicks on markers

--- app/components/TripMap.vue
+++ app/components/TripMap.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useMap } from "@indoorequal/vue-maplibre-gl";
 import type { inferRouterOutputs } from "@trpc/server";
 import type { LngLatLike } from "maplibre-gl";
 import type { AppRouter } from "~~/server/trpc/routers";
@@ -6,26 +7,41 @@ import type { AppRouter } from "~~/server/trpc/routers";
 type RouterOutput = inferRouterOutputs<AppRouter>;
 type TripOutput = NonNullable<RouterOutput["trips"]["trip"]>;
 
-defineProps<{
-  trip: TripOutput | undefined | null;
-}>();
+const props = defineProps<{ trip: TripOutput | undefined | null }>();
 
 const maptilerKey = useRuntimeConfig().public.maptilerKey;
 const style = `https://api.maptiler.com/maps/streets/style.json?key=${maptilerKey}`;
 const center: LngLatLike = [-1.559482, 47.21322];
 const zoom = 8;
+
+const mapKey = `trip-${props.trip?.eqId}`;
+const map = useMap(mapKey);
+
+watch(
+  () => map.isLoaded,
+  (isLoaded) => {
+    console.log("map loaded ", isLoaded);
+  },
+);
+
+const handleMarkerClick = (e: MouseEvent, checkin: TripOutput["checkins"][number]) => {
+  e.stopPropagation();
+  console.log(e, checkin);
+};
 </script>
 
 <template>
   <div v-if="trip" class="h-96 w-full">
     <ClientOnly>
-      <MglMap :map-style="style" :center="center" :zoom="zoom">
+      <MglMap :map-style="style" :center="center" :zoom="zoom" :map-key="mapKey">
         <MglNavigationControl />
         <MglMarker
           v-for="checkin in trip.checkins"
           :key="checkin.eqId"
           :coordinates="[checkin.venue.lng, checkin.venue.lat]"
-        />
+        >
+          <template #marker><div @click="(e) => handleMarkerClick(e, checkin)">📍</div></template>
+        </MglMarker>
       </MglMap>
     </ClientOnly>
   </div>
--- app/pages/trips/[trip].vue
+++ app/pages/trips/[trip].vue
@@ -19,7 +19,9 @@ useSiteHead(page.value);
 <template>
   <div class="prose-custom">
     <ContentRenderer v-if="page" :value="page" />
-    <TripMap :trip="data" />
+    <ClientOnly>
+      <TripMap :trip="data" />
+    </ClientOnly>
     <div v-if="data && data.checkins.length">
       <h2>Checkins</h2>
       <ul>
